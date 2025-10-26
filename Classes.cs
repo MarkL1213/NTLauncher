75b7fa541013b18589def92c76bb82fba9803f9b
Mark Loftis <loftismark1@gmail.com>
2025-10-26 04:30:43 -0700

    Initial Project

---
+++ Classes.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NinjaTraderLauncher
+{
+    public class StartupWorkspace
+    {
+        public string WorkspaceName { get; set; }
+    }
+
+    public class WorkspaceFile
+    {
+        public string FilePath { get; set; }
+
+        public string SetStartupWorkspace(StartupWorkspace workspace)
+        {
+            if (!File.Exists(FilePath))
+            {
+                return "File does not exist";
+            }
+
+            try
+            {
+                string[] lines = File.ReadAllLines(FilePath);
+                int lineIndex = -1;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].StartsWith("  <ActiveWorkspace>", StringComparison.CurrentCulture))
+                    {
+                        lineIndex = i;
+                        break;
+                    }
+                }
+                if (lineIndex == -1)
+                {
+                    return "ActiveWorkspace tag not found in file";
+                }
+                lines[lineIndex] = $"  <ActiveWorkspace>{workspace.WorkspaceName}</ActiveWorkspace>";
+
+                File.WriteAllLines(FilePath, lines);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return string.Empty;
+        }
+
+        public string LookupCurrentWorkspace()
+        {
+            if (!File.Exists(FilePath))
+            {
+                return string.Empty;
+            }
+            try
+            {
+                string[] lines = File.ReadAllLines(FilePath);
+                foreach (string line in lines)
+                {
+                    if (line.StartsWith("  <ActiveWorkspace>", StringComparison.CurrentCulture))
+                    {
+                        int startIndex = line.IndexOf('>') + 1;
+                        int endIndex = line.IndexOf("</ActiveWorkspace>");
+                        if (startIndex >= 0 && endIndex > startIndex)
+                        {
+                            return line.Substring(startIndex, endIndex - startIndex);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return string.Empty;
+            }
+            return string.Empty;
+        }
+    }   
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NinjaTraderLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WorkspaceFile workspaceFile = new WorkspaceFile() { FilePath= "C:\\Users\\Mark\\Documents\\NinjaTrader 8\\workspaces\\_Workspaces.xml" };
+        StartupWorkspace codeworkWorkspace = new StartupWorkspace() { WorkspaceName = "Code Work" };
+        StartupWorkspace tradingWorkspace = new StartupWorkspace() { WorkspaceName = "Trading" };
+        string NinjaTraderExecutable = "C:\\Program Files\\NinjaTrader 8\\bin\\NinjaTrader.exe";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string currentWorkspace = workspaceFile.LookupCurrentWorkspace();
+            if (currentWorkspace == codeworkWorkspace.WorkspaceName)
+            {
+                CodeWorkButton.IsChecked = true;
+            }
+            else if (currentWorkspace == tradingWorkspace.WorkspaceName)
+            {
+                TradingRadioButton.IsChecked = true;
+            }
+        }
+
+        private void LaunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool started = false;
+            using (System.Diagnostics.Process pProcess = new System.Diagnostics.Process())
+            {
+                pProcess.StartInfo.FileName = NinjaTraderExecutable;
+                pProcess.StartInfo.UseShellExecute = false;
+                started = pProcess.Start();
+            }
+            if (started)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void CodeWorkButton_Checked(object sender, RoutedEventArgs e)
+        {
+            string result = workspaceFile.SetStartupWorkspace(codeworkWorkspace);
+            if (!string.IsNullOrEmpty(result))
+            {
+                MessageBox.Show($"Error setting startup workspace to Code Work: {result}");
+            }
+        }
+
+        private void TradingRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            string result = workspaceFile.SetStartupWorkspace(tradingWorkspace);
+            if (!string.IsNullOrEmpty(result))
+            {
+                MessageBox.Show($"Error setting startup workspace to Trading: {result}");
+            }
+        }
+    }
+}
